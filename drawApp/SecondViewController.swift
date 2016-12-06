fc6eb647ce91cd4984c7ad751b2261c92d2df548
Anna Lucking <annavlucking@hotmail.co.uk>
2016-12-06 15:44:47 +0000

    added drawing functionality to secondview controller

--- drawApp/SecondViewController.swift
+++ drawApp/SecondViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet var drawPage: UIImageView!
+    //change to imageView
    
     var lastPoint = CGPoint.zero
     var moved = false
@@ -22,6 +23,53 @@ class SecondViewController: UIViewController {
     }
 
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moved = false
+        
+        if let touch = touches.first {
+           lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawPicture(fromPoint:CGPoint, toPoint:CGPoint) {
+        UIGraphicsBeginImageContextWithOptions(self.view.frame.size, false, 0.0)
+        drawPage.image?.draw(in: CGRect(x: 0, y:0, width:self.view.frame.width, height:self.view.frame.height))
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        
+        context?.setBlendMode(CGBlendMode.color)
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(5)
+        context?.setStrokeColor(UIColor(red: 0.26, green: 0.53, blue: 0.96, alpha: 1.0).cgColor)
+        
+        context?.strokePath()
+        
+        drawPage.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moved = true
+        
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self.view)
+            drawPicture(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            lastPoint = currentPoint
+            
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !moved {
+            drawPicture(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
