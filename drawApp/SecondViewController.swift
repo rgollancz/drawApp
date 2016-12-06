f11c978de55812902c921ae0112f5f71d51ac6bf
Rosie <r.gollancz@gmail.com>
2016-12-06 14:13:42 +0000

    Add draw page

--- drawApp/SecondViewController.swift
+++ drawApp/SecondViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var drawPage: UIImageView!
+   
+    var lastPoint = CGPoint.zero
+    var moved = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
