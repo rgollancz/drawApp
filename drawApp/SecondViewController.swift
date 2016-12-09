05ba43d078f337523306ec7a551734dc032895b7
Rosie <r.gollancz@gmail.com>
2016-12-09 11:37:29 +0000

    Add initial draw from coordinate function

--- drawApp/SecondViewController.swift
+++ drawApp/SecondViewController.swift
@@ -10,11 +10,79 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var picturePage: UIImageView!
+    
+    var coordinatesArray = [DrawingCoordinate]()
+    var fromCo = (153.0, 180.0)
+    var toCo = (173.0, 180.0)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.drawCoordinate()
     }
+
+    
+    struct DrawingCoordinate {
+        var from: CGPoint
+        var to: CGPoint
+        init(from: CGPoint, to: CGPoint) {
+            self.from = from
+            self.to = to
+        }
+    }
+    
+  
+    
+    func drawCoordinate() {
+        UIGraphicsBeginImageContextWithOptions(self.picturePage.bounds.size, false, 0.0)
+            picturePage.image?.draw(in: CGRect(x: 0, y:0, width:self.picturePage.bounds.width, height:self.picturePage.bounds.height))
+
+        let context = UIGraphicsGetCurrentContext()
+        context?.move(to: CGPoint(x: fromCo.0, y: fromCo.1))
+        context?.addLine(to: CGPoint(x: toCo.0, y: toCo.1))
+     
+        context?.setBlendMode(CGBlendMode.color)
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(5)
+        context?.setStrokeColor(UIColor(red: 0.26, green: 0.53, blue: 0.96, alpha: 1.0).cgColor)
+        
+        context?.strokePath()
+        picturePage.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    
+//    func createPicture() {
+//        for coordinate: DrawingCoordinate in coordinatesArray {
+//        drawPicture(coordinate: DrawingCoordinate)
+//    
+//    
+//        }
+//    }
+    
+    
+//    func drawPicture(coordinate:DrawingCoordinate) {
+//        UIGraphicsBeginImageContextWithOptions(self.picturePage.bounds.size, false, 0.0)
+//        picturePage.image?.draw(in: CGRect(x: 0, y:0, width:self.picturePage.bounds.width, height:self.picturePage.bounds.height))
+//        let context = UIGraphicsGetCurrentContext()
+//        
+//        context?.move(to: CGPoint(x: DrawingCoordinate.from, y: DrawingCoordinate.from))
+//        context?.addLine(to: CGPoint(x: DrawingCoordinate.to, y: DrawingCoordinate.to))
+//        
+//        context?.setBlendMode(CGBlendMode.color)
+//        context?.setLineCap(CGLineCap.round)
+//        context?.setLineWidth(5)
+//        context?.setStrokeColor(UIColor(red: 0.26, green: 0.53, blue: 0.96, alpha: 1.0).cgColor)
+//        
+//        context?.strokePath()
+//        
+//        picturePage.image = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//    }
+//    
 
     
     override func didReceiveMemoryWarning() {
