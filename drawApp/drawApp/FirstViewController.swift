98891dc6e26c4fc2802e4325df21b2b913ed19ac
Rosie <r.gollancz@gmail.com>
2016-12-12 16:39:52 +0000

    Non-erroring Starscream code

--- drawApp/drawApp/FirstViewController.swift
+++ drawApp/drawApp/FirstViewController.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2016 RAKE. All rights reserved.
 //
 
+import Starscream
 import UIKit
 
-class FirstViewController: UIViewController, UITextFieldDelegate {
+final class FirstViewController: UIViewController, UITextFieldDelegate, WebSocketDelegate {
     
+    let socket = WebSocket(url: URL(string: "ws://localhost:3000/")!)
     
     //MARK: Properties
     
@@ -21,7 +23,23 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        socket.delegate = self
+        socket.connect()
         nameText.delegate = self
+    }
+
+    func websocketDidConnect(_ socket: WebSocket) {
+        print("websocket is connected");
+    }
+    
+    func websocketDidDisconnect(_ socket: WebSocket, error: NSError?) {
+        socket.write(string: "disconnected");
+        print("websocket disconnected");
+    }
+
+    
+    func websocketDidReceiveData(_ socket: WebSocket, data: Data) {
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -39,10 +57,7 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    
     //MARK: UITextFieldDelegate
-    
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
@@ -60,3 +75,38 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         performSegue(withIdentifier: "showDrawViewController", sender: sender )
     }
 }
+
+
+// MARK: - FilePrivate
+fileprivate extension FirstViewController {
+    
+    func sendDrawing(_ message: String) {
+        socket.write(string: message)
+    }
+    
+    func drawingReceived(_ message: String) {
+        return
+    }
+}
+
+
+
+
+//func websocketDidReceiveMessage(_ socket: WebSocket, text: String) {
+//    // 1
+//    guard let data = text.data(using: .utf16),
+//        let jsonData = try? JSONSerialization.jsonObject(with: data),
+//        let jsonDict = jsonData as? [String: Any],
+//        let messageType = jsonDict["type"] as? String else {
+//            return
+//    }
+//    
+//    // 2
+//    if messageType == "message",
+//        let messageData = jsonDict["data"] as? [String: Any],
+//        let messageAuthor = messageData["author"] as? String,
+//        let messageText = messageData["text"] as? String {
+//        
+//        //            drawingReceived(messageText)
+//    }
+//}
